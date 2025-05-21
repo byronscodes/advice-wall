9ddc0fd48e0929ce2f90861a8ea872b7886dc0bd
Byron Streit <streitb2020@gmail.com>
2025-05-20 20:53:27 -0400

    Basic title and plus button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,29 +15,23 @@ const firebaseConfig = {
   storageBucket: "advice-wall.firebasestorage.app",
   messagingSenderId: "196555767023",
   appId: "1:196555767023:web:1e6ca4d9544c039b37ef1a",
+  databaseURL: "https://advice-wall-default-rtdb.firebaseio.com",
   measurementId: "G-TNLPNPEH6J"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="titleDiv">
+          <a href="#" className="title">Advice Wall</a>
+        </div>
+        <button className="add"><i className="material-symbols-outlined">add</i></button>
       </header>
     </div>
   );
